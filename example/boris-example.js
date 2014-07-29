0d1269c0be1f182b635f900d377a76d010f03cc4
rootslab <44gatti@gmail.com>
2014-07-29 18:05:26 +0200

    corrected example.. On branch master modified:   example/boris-example.js

--- example/boris-example.js
+++ example/boris-example.js
@@ -30,7 +30,7 @@
  * - other test strings:
  *
  *    "*3\r\n$9\r\nsubscribe\r\n$8\r\nchannel1\r\n:1\r\n*3\r\n$9\r\nsubscribe\r\n$8\r\nchannel2\r\n:2\r\n*3\r\n$9\r\nsubscribe\r\n$8\r\nchannel3\r\n:3\r\n*3\r\n$9\r\nsubscribe\r\n$8\r\nchannel1\r\n:3\r\n"
- *	  "*4\r\n$8\r\npmessage\r\n$9\r\n*-channel\r\n$12\r\nfake-channel\r\n$12\r\nHello Fakes!\r\n"
+ *    "*4\r\n$8\r\npmessage\r\n$9\r\n*-channel\r\n$12\r\nfake-channel\r\n$12\r\nHello Fakes!\r\n"
  */
 
 var log = console.log
@@ -43,7 +43,7 @@ var log = console.log
     , integer = new Buffer( ":1\r\n" )
     , multibulk = new Buffer( "*3\r\n$3\r\nted\r\n$3\r\nbob\r\n$5\r\nalice\r\n" )
     , smultibulk = new Buffer( "*3\r\n$9\r\nsubscribe\r\n$8\r\nchannel1\r\n:1\r\n*3\r\n$9\r\nsubscribe\r\n$8\r\nchannel2\r\n:2\r\n*3\r\n$9\r\nsubscribe\r\n$8\r\nchannel3\r\n:3\r\n*3\r\n$9\r\nsubscribe\r\n$8\r\nchannel1\r\n:3\r\n" )
-    , nmultibulk = new Buffer( "*2\r\n*4\r\n:1\r\n:1398634705\r\n:15137\r\n*4\r\n$6\r\nLRANGE\r\n$4\r\nlist\r\n$1\r\n0\r\n$5\r\n16000\r\n*4\r\n:0\r\n:1398634583\r\n:14186\r\n*4\r\n$6\r\nLRANGE\r\n$4\r\nlist\r\n$1\r\n0\r\n$5\r\n16000\r\n" )
+    , nmultibulk = new Buffer( "*4\r\n*4\r\n:1\r\n:1398634705\r\n:15137\r\n*4\r\n$6\r\nLRANGE\r\n$4\r\nlist\r\n$1\r\n0\r\n$5\r\n16000\r\n*4\r\n:0\r\n:1398634583\r\n:14186\r\n*4\r\n$6\r\nLRANGE\r\n$4\r\nlist\r\n$1\r\n0\r\n$5\r\n16000\r\n*4\r\n:1\r\n:1398634705\r\n:15137\r\n*4\r\n$6\r\nLRANGE\r\n$4\r\nlist\r\n$1\r\n0\r\n$5\r\n16000\r\n*4\r\n:0\r\n:1398634583\r\n:14186\r\n*4\r\n$6\r\nLRANGE\r\n$4\r\nlist\r\n$1\r\n0\r\n$5\r\n16000\r\n" )
     ;
 
 b.on( 'end', function () {
@@ -55,30 +55,30 @@ b.on( 'miss', function ( r, i ) {
 } );
 
 b.on( 'match', function ( e, d, convert ) {
-    log( '\n- data match%s: %j"', e ? ' (Redis error)' : '', convert( d ) );
+    log( '  -> match%s: %j"', e ? ' (Redis error)' : '', convert( d ) );
 } );
 
 log( '\n- run all parser rules using a single chunk of data.' );
 
-log( 'parse "+" status reply: %s', status );
+log( '- parse "+" status reply: %j', status + '' );
 b.parse( status );
 
-log( 'parse error "-" reply: %s', status );
+log( '- parse error "-" reply: %j', error + '' );
 b.parse( error );
 
-log( 'parse "$" bulk reply: %s', status );
+log( '- parse "$" bulk reply: %j', bulk + '' );
 b.parse( bulk );
 
-log( 'parse ":" integer reply: %s', status );
+log( '- parse ":" integer reply: %j', integer + '' );
 b.parse( integer );
 
-log( 'parse "*" multibulk reply: %s', status );
+log( '- parse "*" multibulk reply: %j', multibulk + '' );
 b.parse( multibulk );
 
-log( 'parse "*" (SUBSCRIBE) multibulk reply: %s', status );
+log( '- parse "*" (SUBSCRIBE) multibulk reply: %j', smultibulk + '' );
 b.parse( smultibulk );
 
-log( 'parse "*" (SLOWLOG) nested multibulk reply: %s', status );
+log( '- parse "*" (SLOWLOG) nested multibulk reply: %j', nmultibulk + '' );
 b.parse( nmultibulk );
 
 log( '\n- run some parser rules using multiple chunks of data.' );
@@ -89,3 +89,4 @@ b.parse( error.slice( 13 ) );
 
 b.parse( nmultibulk.slice( 0, 13 ) );
 b.parse( nmultibulk.slice( 13 ) );
+b.parse( nmultibulk );
